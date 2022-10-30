476db63df5abeed75bc9a0eb64c39ba8b12c5e82
paveldolgov4 <dolgoff1272@gmail.com>
2022-10-30 23:15:47 +0300

    Style fix + Box fun

--- app/src/main/java/com/example/composelist/Screen.kt
+++ app/src/main/java/com/example/composelist/Screen.kt
@@ -1,8 +1,6 @@
 package com.example.composelist
 
-import android.content.ContentValues.TAG
 import android.content.res.Configuration
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -11,78 +9,45 @@ import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.integerResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.composelist.ui.theme.*
 
-/*@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-@Composable
-fun ShowList(){
-    val numbers = listOf<Int>(1,2,3,4,5,6,7,8,9)
-    ItemsGrid(numbers)
-}*/
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ItemsGrid(/* numbers: List<Int>*/listViewModel: HomeViewModel) {
-    val boxModifier = Modifier
-        .padding(5.dp)
-        .size(integerResource(id = R.integer.NumberBoxSize).dp)
-
+fun ItemsGrid(listViewModel: HomeViewModel) {
     val numbers = listViewModel.numbers
     val configuration = LocalConfiguration.current
-    var gridWeight = (stringResource(id = R.string.GridPortraitWeight)).toFloat()
-    var buttonWeight = (stringResource(id = R.string.BtnPortraitWeight)).toFloat()
-    var numbersAmount = integerResource(id = R.integer.portrait)
+    var gridWeight = GridPortraitWeight
+    var buttonWeight = BtnPortraitWeight
+    var numbersAmount = portrait
     if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE ){
-        gridWeight = (stringResource(id = R.string.GridLandscapeWeight)).toFloat()
-        buttonWeight = (stringResource(id = R.string.BtnLandscapeWeight)).toFloat()
-        numbersAmount = integerResource(id = R.integer.landscape)
+        gridWeight = GridLandscapeWeight
+        buttonWeight = BtnLandscapeWeight
+        numbersAmount = landscape
     }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         LazyVerticalGrid(
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            contentPadding = PaddingValues(VertPadding.dp, HorPadding.dp),
             cells = GridCells.Fixed(numbersAmount),
             modifier = Modifier.weight(gridWeight),
         ) {
             items(numbers.size){ index ->
-                if (numbers[index]%2==1){
-                    // Вынести вызов Box в функцию
-                    Box(
-                        boxModifier
-                            .background(Color.Blue),
-                        contentAlignment = Alignment.Center
-                    ){
-                        Text("${numbers[index]}",textAlign = TextAlign.Center)
-                    }
-                }else{
-                    Box(
-                        boxModifier
-                            .background(Color.Red),
-                        contentAlignment = Alignment.Center
-
-                    ){
-                        Text("${numbers[index]}",textAlign = TextAlign.Center)
-                    }
-                }
+                if (numbers[index]%2==1) MakeBox(BlueBoxClr, numbers[index])
+                else MakeBox(RedBoxClr, numbers[index])
             }
         }
         Button(
             onClick = { listViewModel.addElement() },
             modifier = Modifier
-                .padding(7.dp)
-                .widthIn(min = integerResource(id = R.integer.ButtonSize).dp)
+                .padding(ButtonPadding.dp)
+                .widthIn(ButtonSize.dp)
                 .weight(buttonWeight),
         )
         {
@@ -94,48 +59,21 @@ fun ItemsGrid(/* numbers: List<Int>*/listViewModel: HomeViewModel) {
         }
     }
 }
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Screen_v2(){
-    val buttonModifier = Modifier
-    val numbers = remember{
-        mutableStateListOf(emptyList<Int>())
-    }
-    val configuration = LocalConfiguration.current
-    val numbersAmount = when (configuration.orientation) {
-        Configuration.ORIENTATION_LANDSCAPE -> {
-            integerResource(id = R.integer.landscape)
-        }
-        else -> {
-            integerResource(id = R.integer.portrait)
-        }
-    }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+fun MakeBox(color: Color, number: Int){
+    val boxModifier = Modifier
+        .padding(BoxPadding.dp)
+        .size(NumberBoxSize.dp)
+    Box(
+        boxModifier
+            .background(color),
+        contentAlignment = Alignment.Center
+
     ){
-        LazyVerticalGrid(
-            cells = GridCells.Fixed(numbersAmount)
-        ) {
-            items(numbers.size){ index ->
-                    Box(
-                        buttonModifier
-                            .size(integerResource(id = R.integer.NumberBoxSize).dp)
-                            .background(Color.Red)
-                    ){
-                        Text("${numbers[index]}")
-                        Log.d(TAG, "Вывод ${numbers[index]}")
-                    }
-                }
-            }
-        Button(onClick = {numbers.add(listOf(numbers.lastIndex + 1))},
-            modifier = Modifier
-                .padding(5.dp)
-                .size(integerResource(id = R.integer.ButtonSize).dp),)
-        {
-            Text("Add box")
-        }
-        }
+        Text("$number",textAlign = TextAlign.Center)
+    }
 }
+
 
     
  
---
+++ app/src/main/java/com/example/composelist/ui/theme/Dimens.kt
@@ -0,0 +1,14 @@
+package com.example.composelist.ui.theme
+
+const val NumberBoxSize = 50
+const val ButtonSize = 50
+const val GridLandscapeWeight = 0.8F
+const val GridPortraitWeight = 0.9F
+const val BtnLandscapeWeight = 0.2F
+const val BtnPortraitWeight = 0.1F
+const val portrait = 3
+const val landscape = 4
+const val HorPadding = 16
+const val VertPadding = 8
+const val ButtonPadding = 8
+const val BoxPadding = 4
